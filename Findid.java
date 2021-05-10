01604262d37dd6bba5052ee1fe611ffb1063cddd
Banghyoyeon <70623062+Banghyoyeon@users.noreply.github.com>
2021-05-10 22:17:05 +0900

    Add files via upload
    210510
    로그인 / 회원가입 / ID 찾기 / PW 찾기 화면
    - 방효연 -

---
+++ Findid.java
@@ -0,0 +1,14 @@
+package com.project.whichwitch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Findid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_findid);
+    }
+}
--- Joinus.java
+++ Joinus.java
@@ -1,38 +1,42 @@
 package com.project.whichwitch;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class Joinus extends AppCompatActivity {
-    Button joinus_joinus_btn, joinus_login_btn;
+    Button joinus_joinus_btn;
+    Button joinus_login_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joinus);
-
         joinus_joinus_btn=(Button)findViewById(R.id.joinus_joinus_btn);
-        joinus_joinus_btn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                AlertDialog.Builder dlg=new AlertDialog.Builder(Joinus.this);
-                dlg.setTitle("알림");
-                dlg.setMessage("회원가입이 완료되었습니다.");
-                dlg.setPositiveButton("확인",null);   // null -> Intent
-                dlg.show();
-            }
-        });
+        joinus_joinus_btn.setOnClickListener(new View.OnClickListener() {
+                 @Override
+                 public void onClick(View v) {
+                     AlertDialog.Builder dlg = new AlertDialog.Builder(Joinus.this);
+                     dlg.setTitle("알림");
+                     dlg.setMessage("회원가입이 완료되었습니다.");
+                     dlg.setPositiveButton("확인", null);
+                     dlg.show();
+                 }
+             }
+        );
+
         joinus_login_btn=(Button)findViewById(R.id.joinus_login_btn);
-        joinus_login_btn.setOnClickListener(new View.OnClickListener(){
+        joinus_login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 Intent intent=new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+                setResult(RESULT_OK,intent);
+                finish();
             }
         });
     }
